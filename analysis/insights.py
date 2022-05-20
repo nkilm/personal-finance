1c51c420e0e25d6c30c33ff442c67362e853cc1f
nkilm <nikhilmohitelhs@gmail.com>
2022-05-21 01:12:33 +0530

    month and year

--- analysis/insights.py
+++ analysis/insights.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 
 df = pd.read_csv("./data/transactions.csv")
 
@@ -15,4 +16,19 @@ def summary():
     credits = df["Credit"].sum()
     debits = df["Debit"].sum()
     return credits,debits
-    
+
+def present_date():
+    date = df["Transaction Date"][len(df["Transaction Date"])-1]
+    months_in_year = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    datem = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
+    month = months_in_year[datem.month-1]
+    year = datem.year
+    return month,year
+
+def balance():
+    date= df["Transaction Date"][len(df["Transaction Date"])-1]
+    date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
+    date = f"{date.day}-{date.month}-{date.year}"
+    bal = df["Balance"][len(df["Balance"])-1]
+
+    return date,bal
--- app.py
+++ app.py
@@ -2,14 +2,22 @@ import streamlit as st
 import pandas as pd
 from analysis.insights import *
 
-st.write("<h1 style=\"color:#fff;text-align:center;\">My Finances</h1>",unsafe_allow_html=True)
+st.write("<h1 style=\"margin-bottom:70px;border-bottom:1px solid red\">My Finances</h1>",unsafe_allow_html=True)
 
 navbar = st.sidebar.radio("Index",["Summary","Transactions","Graphs"])
 
 if navbar=="Summary":
     c,d = summary()
-    st.write(f"<h3>CREDIT &nbsp&nbsp<span style=\"color:#67AE45\">₹{c}</span></h3>",unsafe_allow_html=True)
-    st.write(f"<h3>DEBIT <span style=\"color:#CA6B6B;padding-left:30px\">₹{d}</span></h3>",unsafe_allow_html=True)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write(f"<h3>CREDIT &nbsp&nbsp<span style=\"color:#67AE45;padding-left:30px\">₹{c}</span></h3>",unsafe_allow_html=True)
+        st.write(f"<h3>DEBIT <span style=\"color:#CA6B6B;padding-left:60px\">₹{d}</span></h3>",unsafe_allow_html=True)
+    with col2: 
+        month,year = present_date()
+        st.write(f"<h3 style=\"text-align:right\">{month.upper()} {year}</h3>",unsafe_allow_html=True)
+
+    d,b = balance()
+    st.write(f"<h3 style=\"border-top:1px solid red;margin-top:80px;padding-top:50px\">BALANCE <span style=\"color:yellow;padding-left:20px;\">₹{b}</span></h3>",unsafe_allow_html=True)
 
 if navbar=="Transactions":
     st.dataframe(transactions_list())
