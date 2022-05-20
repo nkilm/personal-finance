b7538ebc96a4803aa3aac816347c8ce77f959972
nkilm <nikhilmohitelhs@gmail.com>
2022-05-21 01:12:33 +0530

    month and year

--- analysis/insights.py
+++ analysis/insights.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 
 df = pd.read_csv("./data/transactions.csv")
 
@@ -15,4 +16,13 @@ def summary():
     credits = df["Credit"].sum()
     debits = df["Debit"].sum()
     return credits,debits
-    
+
+def month():
+    date = df["Transaction Date"][len(df["Transaction Date"])-1]
+    months_in_year = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+    print(date)
+    datem = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
+    month = months_in_year[datem.month-1]
+    year = datem.year
+    return month,months_in_year
